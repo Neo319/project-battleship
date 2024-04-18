b4dd35a911afcf3e74e3ad82bce0bb38ef301849
CastorDisaster <alexmnevins@gmail.com>
2024-04-18 13:18:13 -0400

    create first gameboard tests

---
+++ game_logic/gameboard.js
@@ -0,0 +1,18 @@
+import Ship from "./ship";
+
+export default class Gameboard {
+    constructor() {
+        this.allShipsSunk = false;
+        this.ships = []; //array of objects
+    }
+
+    //using inputted coordinates as the leftmost space
+    placeShipHorizontal (x, y, length) {
+
+    }
+
+    //inputted coordinates are top space
+    placeShipVertical(x, y, length) {
+
+    }
+}
---
+++ game_logic/gameboard.test.js
@@ -0,0 +1,14 @@
+import Gameboard from "./gameboard";
+import Ship from "./ship";
+
+//first steps
+test('gameboard exists', () => {
+    expect(Gameboard).toBeDefined() 
+});
+
+describe('gameboard functionality', () => {
+    //place ship at a coordinate
+    
+    //test if attacked coordinate
+
+})
