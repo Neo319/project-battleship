d8f8b40bc72ef3a5726c65dcaca6da0a6322fed2
CastorDisaster <alexmnevins@gmail.com>
2024-04-21 11:01:22 -0400

    add function for displaying board grid

---
+++ DOM_logic/board_displayer.js
@@ -0,0 +1,10 @@
+//module responsible for displaying board content to DOM
+export default function boardDisplayer(type, location) {
+    //board can be of type 'player' or 'cpu', 
+    //changing what is displayed.
+    //'location determines to which div the board is generated'
+
+    location.textContent = "here!"
+
+
+}
--- DOM_logic/gameManager.js
+++ DOM_logic/gameManager.js
@@ -1,9 +1,37 @@
 import Player from "../game_logic/player.js";
+import boardDisplayer from "./board_displayer.js";
 
 const gameManager = function () {
     console.log("foo");
 
-    
+    //initial game setup
+
+    const player = new Player(1); 
+    const cpu = new Player(2);
+
+    const playerBoardAddress = document.getElementById("playerBoard");
+    const cpuBoardAddress = document.getElementById("cpuBoard");
+
+    //to begin with, use predetermined coordinates
+    function _initialLayout(player) {
+
+        player.board.placeShipVertical(2, 4, 2);
+
+        player.board.placeShipHorizontal(1, 0, 3);
+        player.board.placeShipVertical(8, 3, 3);
+
+        player.board.placeShipVertical(4, 5, 4);
+
+        player.board.placeShipHorizontal(1, 2, 5);
+
+    }
+
+    _initialLayout(player);
+    _initialLayout(cpu);
+
+    boardDisplayer(1, playerBoardAddress)
+
+
 }
 
 export default gameManager;
