b61b2cd6b283c8de995b43edda50b0a32125176c
CastorDisaster <alexmnevins@gmail.com>
2024-04-21 19:51:55 -0400

    implement reset button

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import gameManager from "./DOM_logic/gameManager.js";
 import Player from "./game_logic/player.js";
 
 //player objects that will be passed around
-const player = new Player(1); 
-const cpu = new Player(2);
+let player = new Player(1); 
+let cpu = new Player(2);
 
 //buttons
 const start = document.getElementById('start')
@@ -24,9 +24,27 @@ start.addEventListener('click', () => {
 });
 reset.addEventListener('click', () => {
     if (!(reset.classList.contains("inactive")))
-    commandButtons("reset");
-    layout.classList = 'actice';
+    
+    layout.classList = 'active';
     start.classList = 'active';
+    reset.classList = "inactive";
+
+    //reset game state from here :
+
+    //create new player objects
+    player = new Player(1);
+    cpu = new Player(2);
+
+    //reset display
+    let cpuBoard = document.getElementById("cpuBoard");
+    cpuBoard.innerHTML = ''; //clear board until 'start' is pushed
+
+    //generate new player board
+    commandButtons('layout', player, cpu);
+
+
+
+
 });
 layout.addEventListener('click', () => {
     if (!(layout.classList.contains("inactive"))) {
